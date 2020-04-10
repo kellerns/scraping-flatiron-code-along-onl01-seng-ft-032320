c85030db040e062dfcfaddacce00bf629a70b8bb
Nicholas Keller <nic.keller328@gmail.com>
2020-04-10 20:59:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,7 @@ class Scraper
 
       binding.pry
   end
-  
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -21,8 +21,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
